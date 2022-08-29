de386b8f11bb0b162804bd554409a039f1ee9a5f
chandan <chandansutradhar2@gmail.com>
2022-08-29 14:53:55 +0530

    added subtract logic

--- demo.js
+++ demo.js
@@ -3,5 +3,10 @@ function add(a,b){
 }
 
 function subtract(a,b){
-    return a-b;
+    //todo:check if a and b is a number
+    return typeof(a)=="number" && typeof(b)=='number'?
+    a-b:'please enter number only';
+
 }
+
+console.log(subtract(10,30));
